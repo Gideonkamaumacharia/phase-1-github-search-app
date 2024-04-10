b9b9a8726d98db520840da50bec30e972b680931
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-10 13:34:47 +0300

    commit changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-let inputValue = document.getElementById("search").addEventListener("submit",()=>{
-    fetch("")
-})
+let inputValue = document.getElementById("search")
+
+inputValue.addEventListener("submit", fetchUsers)
+
+function fetchUsers(e){
+    let searchItem = inputValue.value
+    e.preventDefault()
+    fetch(`https://api.github.com/search/users?q=${inputValue}`)
+    .then((res)=> res.json())
+    .then(data => data.items.forEach(user => createList(data.items)))
+    }
+function createList(users){
+    let ul = document.getElementById("user-list")
+    let li = document.createElement("li")
+    li.innerText = inputValue
+    ul.appendChild(li)
+
+
+}
